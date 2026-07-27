6ce895967116fa092a1f4a1eff87a05c10b362f3
Catanalyst <cataanorambuena@gmail.com>
2026-07-27 14:32:50 -0400

    Se cambia la ruta de acceso a car_data

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv('../vehicles_us.csv')  # leer los datos
+car_data = pd.read_csv(
+    r"C:\Users\cataa\anaconda_projects\Proyectos de práctica\vehicle_app\vehicles_us.csv")
+# leer los datos
 
 # Crear un encabezado
 st.header('Dashboard de análisis de vehículos')
